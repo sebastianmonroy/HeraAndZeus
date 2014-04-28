bfbd7e59413c764f2be9a6983e7266ff2cb37c41
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-04-28 18:40:38 -0400

    Scroll to Hold Up fix
    Holding up a card no longer clipping pane.

--- HeraAndZeus/Assets/Scripts/Card.cs
+++ HeraAndZeus/Assets/Scripts/Card.cs
@@ -244,14 +244,14 @@ public class Card : MonoBehaviour {
 				if (isPickedUp) {	// already picked up
 					if (picking) {		// is being put down
 						isPickedUp = false;
-						pickDestination = Camera.main.transform.position - Vector3.up * 1;
+						pickDestination = Camera.main.transform.position - Vector3.up * 2;
 						desiredScale = 5 * originalScale;
 						picking = true;
 					}
 				} else {			// not already picked up
 					if (!picking) {		// is not being picked up
 						originalPosition = this.transform.position;
-						pickDestination = Camera.main.transform.position - Vector3.up * 1;
+						pickDestination = Camera.main.transform.position - Vector3.up * 2;
 						desiredScale = 5 * originalScale;
 						picking = true;
 					}
